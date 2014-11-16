af6817a480fdc6a7b691cf8f9ce382a1dd240bca
Renan Fichberg <rfichberg@gmail.com>
2014-11-16 03:23:15 -0200

    1) Started to configure created components. This is being done in InputReader. 2) Added new attributes fields to Host and Router objects. 3) Added methods to manipulate a new attribute of Router class objects. These may make our lives easier in the future.

--- Host.java
+++ Host.java
@@ -1,14 +1,38 @@
 public class Host
 {
-	int id;             //identificador do host. Este identificador é único para este host
+	int id;               //identificador do host. Este identificador é único para este host
+	String computer_ip;   //endereço IP do computador
+    String router_ip;     //endereço ip do roteador padrao
+    String dns_server_ip; //endereço ip do servidor dns
+
 
 	//Construtor
 	public Host(int id)
 	{
 		this.id = id;
+		this.computer_ip = null;
+		this.router_ip = null;
+		this.dns_server_ip = null;
 	}
 
-	//setters desnecessarios pois a configuracao e imutavel e ja escolhida no arquivo de entrada.
+	//setters
+	void set_computer_ip(String computer_ip)
+	{
+		if(this.computer_ip == null)
+			this.computer_ip = computer_ip;
+	}
+
+	void set_router_ip(String router_ip)
+	{
+		if(this.router_ip == null)
+			this.router_ip = router_ip;
+	}
+
+	void set_dns_server_ip(String dns_server_ip)
+	{
+		if(this.dns_server_ip == null)
+			this.dns_server_ip = dns_server_ip;
+	}
 
 	//getters
 	public int get_id()
--- Router.java
+++ Router.java
@@ -1,7 +1,11 @@
+import java.util.*;
+import java.io.*;
+
 public class Router
 {
-	int id;             //identificador do router. Este identificador é único para este router
-	int interfaces;     //numero de interfaces que este router tem
+	int id;                       //identificador do router. Este identificador é único para este router
+	int interfaces;               //numero de interfaces que este router tem
+	String ports_and_ips;         //String com os pares de ip / porta.
 
 	//Construtor
 	public Router(int id, int interfaces)
@@ -21,5 +25,118 @@ public class Router
 	public int get_interfaces()
 	{
 		return this.interfaces;
+	}
+
+/*******************************************************************************************************************************************************************/
+	//Funcoes para a manipulacao do atributo String ports_and_ips
+	//Checa se a porta existe no atributo de portas e strings
+	boolean have_port(String port)
+	{
+		String all_ports_and_ips = " " + ports_and_ips;
+		int number_of_spaces = char_counter(' ', all_ports_and_ips);
+		for(int i = 1; i <= number_of_spaces; i += 2) //i pode ir de 2 em 2, ja que estamos procurando portas e nao IPs
+		{
+			String port_from_string;
+			if(i < number_of_spaces)
+				port_from_string = all_ports_and_ips.substring(get_nth_char_index(all_ports_and_ips, ' ', i) + 1, get_nth_char_index(all_ports_and_ips, ' ', i + 1));
+			else
+				port_from_string = all_ports_and_ips.substring(get_nth_char_index(all_ports_and_ips, ' ', i) + 1);
+			if(port_from_string.compareTo(port) == 0) return true;
+		}
+		return false;
+	}
+
+	//Checa se o IP existe no atributo de portas e strings
+	boolean have_ip(String ip)
+	{
+		// 0 192.168.3.4 1 192.168.2.3 2 192.168.1.2
+		String all_ports_and_ips = " " + ports_and_ips;
+		int number_of_spaces = char_counter(' ', all_ports_and_ips);
+		for(int i = 2; i <= number_of_spaces; i += 2) //i pode ir de 2 em 2, ja que estamos procurando IPs e nao portas
+		{
+			String ip_from_string;
+			if(i < number_of_spaces)
+				ip_from_string = all_ports_and_ips.substring(get_nth_char_index(all_ports_and_ips, ' ', i) + 1, get_nth_char_index(all_ports_and_ips, ' ', i + 1));
+			else
+				ip_from_string = all_ports_and_ips.substring(get_nth_char_index(all_ports_and_ips, ' ', i) + 1);
+			if(ip_from_string.compareTo(ip) == 0) return true;
+		}
+		return false;
+	}
+
+	//Busca o IP associado a porta
+	String retrieve_associated_ip(String port)
+	{
+		String all_ports_and_ips = " " + ports_and_ips;
+		int number_of_spaces = char_counter(' ', all_ports_and_ips), i;
+		boolean found = false;
+		for(i = 1; i <= number_of_spaces; i += 2) //i pode ir de 2 em 2, ja que estamos procurando a partir do IP
+		{
+			String port_from_string;
+			if(i < number_of_spaces)
+				port_from_string = all_ports_and_ips.substring(get_nth_char_index(all_ports_and_ips, ' ', i) + 1, get_nth_char_index(all_ports_and_ips, ' ', i + 1));
+			else
+				port_from_string = all_ports_and_ips.substring(get_nth_char_index(all_ports_and_ips, ' ', i) + 1);
+			if(port_from_string.compareTo(port) == 0) 
+			{
+				found = true; break;
+			}
+		}
+		if(found == true)
+		{
+			if(i < number_of_spaces - 1)
+				return all_ports_and_ips.substring(get_nth_char_index(all_ports_and_ips, ' ', i + 1) + 1, get_nth_char_index(all_ports_and_ips, ' ', i + 2));
+			else
+				return all_ports_and_ips.substring(get_nth_char_index(all_ports_and_ips, ' ', i + 1) + 1);
+		}
+		else
+			return null;
+	}
+
+	//Busca a porta associado ao IP
+	String retrieve_associated_port(String ip)
+	{
+		String all_ports_and_ips = " " + ports_and_ips;
+		int number_of_spaces = char_counter(' ', all_ports_and_ips), i;
+		boolean found = false;
+		for(i = 2; i <= number_of_spaces; i += 2) //i pode ir de 2 em 2, ja que estamos procurando a partir do IP
+		{
+			String ip_from_string;
+			if(i < number_of_spaces)
+				ip_from_string = all_ports_and_ips.substring(get_nth_char_index(all_ports_and_ips, ' ', i) + 1, get_nth_char_index(all_ports_and_ips, ' ', i + 1));
+			else
+				ip_from_string = all_ports_and_ips.substring(get_nth_char_index(all_ports_and_ips, ' ', i) + 1);
+			if(ip_from_string.compareTo(ip) == 0) 
+			{
+				found = true; break;
+			}
+		}
+		if(found == true)
+			return all_ports_and_ips.substring(get_nth_char_index(all_ports_and_ips, ' ', i - 1) + 1, get_nth_char_index(all_ports_and_ips, ' ', i));
+		else
+			return null;
+	}
+
+/*******************************************************************************************************************************************************************/
+	//Metodos privados
+	//Retorna o indice da n-esima ocorrencia nth_char do caracter ch na string line. -1 se nao encontrar o caracter com o numero ocorrencias procurado
+	private int get_nth_char_index(String line, char ch, int nth_char)
+	{
+		int counter = 0;
+		for(int i = 0; i < line.length(); i++)
+		{
+			if(line.charAt(i) == ch) counter++;
+			if(counter == nth_char) return i;
+		}
+		return -1;
+	}
+
+	//Conta o numero de ocorrencias de um caracter ch na string line
+	private int char_counter(char ch, String line)
+	{
+		int counter = 0, i;
+		for(i = 0; i < line.length(); i++)
+			if(line.charAt(i) == ch) counter++;
+		return counter;
 	}
 }
